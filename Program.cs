add3138085bdd8a9f7340cba437a00a4bccf282b
SaulKaique <Saul.silva@etec.sp.gov.br>
2022-10-17 14:51:58 -0300

    segundo da lista da segunda temporada  com poucas modificaçoes

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+
+int N1,N2 ;
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("--- Múltiplo ---\n");
+Console.ResetColor();
+
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("Digite Um numero...:");
+N1 = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.Write("Digite Um numero...:");
+N2 = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+
+if (N1 % N2 == 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine($"\n{N1} È Múltiplo de {N2}");
+    Console.ResetColor();
+
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"\n{N1} Não é Múltiplo de {N2}");
+    Console.ResetColor();
+
+}
